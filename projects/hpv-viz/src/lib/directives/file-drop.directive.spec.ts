d824180a98e32b20721878c1dc4214e89e3e2d7a
David Streid <dcs2153@columbia.edu>
2019-04-27 11:12:35 -0400

    Tests

---
+++ projects/hpv-viz/src/lib/directives/file-drop.directive.spec.ts
@@ -0,0 +1,62 @@
+import { TestBed, ComponentFixture }  from '@angular/core/testing';
+import { Component, DebugElement }    from '@angular/core';
+import { By }                         from '@angular/platform-browser';
+import { FileDropDirective }          from './file-drop.directive';
+@Component({
+  template: `<div
+                fileDrop
+                (filesDropped)='handleDrop($event)'
+                (filesHovered)='handleHover($event)'></div>`
+})
+class TestFileDropComponent{
+  public handleDrop($event): void {}
+  public handleHover($event): void {}
+}
+
+describe('Directive', () => {
+  let component: TestFileDropComponent;
+  let fixture: ComponentFixture<TestFileDropComponent>;
+  let div: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TestFileDropComponent, FileDropDirective ]
+    });
+
+    fixture = TestBed.createComponent(TestFileDropComponent);
+    component = fixture.componentInstance;
+    div = fixture.debugElement.query(By.css('div'));
+  });
+
+  it('filesHovered emits true on hover over', () => {
+    spyOn(component, 'handleHover');
+
+    const evt = new Event('dragOverEvent', {});
+    div.triggerEventHandler('dragover', evt);
+
+    expect(component.handleHover).toHaveBeenCalledWith(true);
+  });
+
+  it('filesHovered emits false on hover leave', () => {
+    spyOn(component, 'handleHover');
+
+    const evt = new Event('dragLeaveEvent', {});
+    div.triggerEventHandler('dragleave', evt);
+
+    expect(component.handleHover).toHaveBeenCalledWith(false);
+  });
+
+  it('On drop event, hover emits false and drop emits files', () => {
+    spyOn(component, 'handleHover');
+    spyOn(component, 'handleDrop');
+
+    const files = 'MOCK'
+    const evt: Event = new Event('dropEvent', {});
+    evt[ 'dataTransfer' ] = { files } ;
+
+    div.triggerEventHandler('drop', evt);
+
+    expect(component.handleDrop).toHaveBeenCalledWith(files);
+    expect(component.handleHover).toHaveBeenCalledWith(false);
+  });
+});
---
+++ projects/hpv-viz/src/lib/file-upload/file-upload.component.spec.ts
@@ -0,0 +1,64 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FileUploadComponent } from './file-upload.component';
+import { TEST_FILES } from '../../../../tests/mock-data/vcf-files';
+
+describe('FileUploadComponent', () => {
+  let component: FileUploadComponent;
+  let fixture: ComponentFixture<FileUploadComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FileUploadComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FileUploadComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Toggles dropzoneState correctly', () => {
+    /**
+     * These events should be emitted by the file-drop directive
+     * EVENTS:
+     *    true:   Should occur when the directive registers hovering files entering
+     *    false:  Shoul occur when the directive registers hovering files leaving or user dropping files
+     */
+    const events = [true, false];
+
+    for( var e of events ){
+      component.dropzoneState(e);
+      expect(component.dropzoneActive).toEqual(e);
+    }
+  });
+
+  it('On drop, vcfUpload emits correct event', (done) => {
+    for( var fileName in TEST_FILES ) {
+      const f = new File([""], "filename.txt");
+      const contents = TEST_FILES[ fileName ]['contents'];
+      const datapoint = TEST_FILES[ fileName ]['datapoint'];
+      const blob = new Blob([contents], {type : 'application/json'});
+      blob['name'] = fileName;
+      const file = <File>blob;
+
+      // Mocking FileList Ref - https://gist.github.com/amabes/88324d68690e0e7b8e313cd0cafaa219
+      const fileList = {
+        0: file,
+        length: 1,
+        item: (index: number) => file
+      };
+
+      component.vcfUpload.subscribe($event => {
+        expect($event).toEqual(datapoint);
+        done();
+      });
+      component.handleDrop(fileList);
+    }
+  })
+});
