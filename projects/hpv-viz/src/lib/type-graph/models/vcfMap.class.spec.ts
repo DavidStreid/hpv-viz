1539fda0293a166d3e5cd99de0ab9016a0df73c5
David Streid <dcs2153@columbia.edu>
2019-06-03 22:20:56 -0400

    Adding new data model for tracking vcf contents

---
+++ projects/hpv-viz/src/lib/type-graph/models/vcfMap.class.spec.ts
@@ -0,0 +1,53 @@
+import { VcfMap } from './vcfMap.class';
+
+describe('VcfMap', () => {
+  const sample = 'MOCK_SAMPLE';
+  const chrom = 'TEST_CHROM';
+  const date: Date = new Date();
+  const data: Object = { 'CHROM': chrom };
+  // TODO - make this a class?
+  const entry = {
+    name: date, // name of the entry is typically the date
+    data: data
+  };
+
+  it('Adding to map creates an entry', () => {
+    const vcfMap = new VcfMap();
+    vcfMap.add(sample, entry);
+    expect(vcfMap.numEntries(chrom)).toBe(1);
+  });
+
+  it('Multipe entries - numEntries, keys, & clear', () => {
+    const chromosomes: string[] = [ 'C1', 'C2', 'C3' ];
+    const vcfMap = new VcfMap();
+
+    /**
+     * Convoluted way of adding 5 entries for c1, 10 entries for c2, & 15 entries fo r c3
+     */
+    for ( const i = 0; i < chromosomes.length; i++) {
+      for ( const j = 0; j < (i + 1) * 5; j++) {
+        const e = JSON.parse(JSON.stringify(entry)); // Need a deep copy since entry is used throughout test file
+        e['data']['CHROM'] = chromosomes[i];
+        e['name'] = new Date(e['name']);
+        vcfMap.add(`s${i}`, e);
+      }
+    }
+
+    // Testing for correct amounts
+    for ( const i = 0; i < chromosomes.length; i++) {
+      expect(vcfMap.numEntries(chromosomes[i])).toBe((i + 1) * 5);
+    }
+
+    expect(vcfMap.keys()).toEqual(chromosomes);
+
+    vcfMap.clear();
+
+    expect(vcfMap.keys()).toEqual([]);
+  });
+
+  it('get retrieves added entry', () => {
+    const vcfMap = new VcfMap();
+    vcfMap.add(sample, entry);
+    expect(vcfMap.get(sample, date, chrom)).toEqual([data]);
+  });
+});
---
+++ projects/hpv-viz/src/lib/type-graph/models/vcfMap.class.ts
@@ -0,0 +1,110 @@
+/**
+ * Class for Map of vcf information
+ *
+ *    vcfMap: {
+ *        chrom: {
+ *            date: {
+ *                sample: [{}, ...]
+ *            }
+ *        }
+ *
+ *    }
+ */
+export class VcfMap {
+  private vcfMap: Map<string, Map<string, any>>;
+
+  constructor() {
+    this.vcfMap = new Map<string, Map<string, any>>();
+  }
+
+  /**
+   * Clears the current state of the map
+   */
+  public clear() {
+    this.vcfMap = new Map<string, Map<string, any>>();
+  }
+
+  private dateKey(date: Date): string {
+    return date.toDateString();
+  }
+
+  public keys(): string[] {
+    return Array.from(this.vcfMap.keys());
+  }
+
+  public numEntries(k: string): number {
+    const dMap: Map<string, Map<string, Object[]>> = this.vcfMap.get(k);
+    if ( dMap === undefined ) { return 0; }
+
+    let numEntries = 0;
+
+    for ( const d of Array.from( dMap.keys() ) ) {
+      const cMap: Map<string, Object[]> = dMap.get(d);
+      for ( const c of Array.from( cMap.keys() ) ) {
+        const entries: Object[] = cMap.get(c);
+        numEntries += entries.length;
+      }
+    }
+
+    return numEntries;
+  }
+
+  /**
+   * Computes a value of the map if it doesn't already exist. If it does exist, returns that value
+   *
+   * @param map, Map - Input map to check for membership
+   * @param key, string - key being queried
+   * @param val, any - value to assign to key if key does not already exist in map
+   */
+  private computeIfAbsent(map: Map<string, any>, key: string, val: any) {
+    if (map.has(key)) {
+      return map.get(key);
+    }
+    map.set(key, val);
+    return map.get(key);
+  }
+
+  /**
+   * Adds an entry to the map
+   *
+   * @param sample, string - sample the entry belongs to
+   * @param entry, Object - enriched object of variant information taken from a line of the vcf
+   */
+  add(sample: string, entry: Object) {
+    const date: Date = entry['name'];
+    const data: Object = entry['data'] || {};
+    const chrom = data['CHROM'];
+
+    if ( sample === undefined || date === undefined || chrom === undefined ) {
+      // Impropertly-formatted object. Return and log error
+      console.error(`Invalid parameters - date: ${date}, sample: ${sample}, data: ${data}, chrom: ${chrom}`);
+      return;
+    }
+
+    const dateKey = this.dateKey(date);
+    const nMap: Map<string, any> = this.computeIfAbsent(this.vcfMap, chrom, new Map<string, any>());
+    const cMap: Map<string, Object[]> = this.computeIfAbsent(nMap, dateKey, new Map<string, Object[]>());
+    const entries: Object[] = this.computeIfAbsent(cMap, sample, []);
+    entries.push(data);
+    cMap.set(sample, entries);
+  }
+
+  /**
+   * Gets map data. To return higher levels of the map, pass implicit parameters as undefined
+   *
+   * @param sample, string - sample of the vcf
+   * @param date, Date - date of the vcf sampling
+   * @param chr, string - Chromosome
+   */
+  get(sample: string, date: Date, chr: string) {
+    const sMap = this.vcfMap.get(chr);
+    if ( this.vcfMap.has(chr) && date !== null ) {
+      const dateKey = this.dateKey(date);
+      const dMap = sMap.get(dateKey);
+      if ( sMap.has(dateKey) && sample !== null ) {
+        return dMap.get(sample);
+      }
+    }
+    return sMap;
+  }
+}
