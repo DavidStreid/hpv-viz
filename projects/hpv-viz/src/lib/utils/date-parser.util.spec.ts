0890d789da08e1c8e595d5876f3ed879d97041ef
David Streid <david.streid.nyc@gmail.com>
2020-04-11 19:50:15 -0500

    Fixing date tests

--- projects/hpv-viz/src/lib/utils/date-parser.util.spec.ts
+++ projects/hpv-viz/src/lib/utils/date-parser.util.spec.ts
@@ -8,28 +8,26 @@ describe('DateParserUtil', () => {
     util = new DateParserUtil();
   }));
 
-  it('\P1_19941028.hpv.bam.vcf\' -> \'Wed Nov 28 1994 00:00:00 GMT-0500\'', () => {
-    const date: Date = util.getDateFromFileName('P1_19941028.hpv.bam.vcf');
-    expect(date).toEqual(new Date('Wed Nov 28 1994 00:00:00 GMT-0500'));
+  const dateCases = [
+    ['P1_19941028.hpv.bam.vcf', 'Wed Oct 28 1994 00:00:00'],   // Not a special case
+    ['P2_9091994.ann.vcf', 'Fri Sep 09 1994 00:00:00'],        // Special case - Need to format "9091994" -> "09091994"
+    ['P3_11121993.ann.vcf', 'Fri Nov 12 1993 00:00:00'],       // Not a special case
+    ['P4.ann.vcf', null]
+  ];
+  it(`${dateCases[0][0]} -> ${dateCases[0][1]}`, () => {
+    const date: Date = util.getDateFromFileName(dateCases[0][0]);
+    expect(date).toEqual(new Date(dateCases[0][1]));
   });
-
-  it('\P1_3161994.ann.vcf\' -> \'Wed Mar 16 1994 00:00:00 GMT-0500\'', () => {
-    const date: Date = util.getDateFromFileName('P1_3161994.ann.vcf');
-    expect(date).toEqual(new Date('Wed Mar 16 1994 00:00:00 GMT-0500'));
+  it(`${dateCases[1][0]} -> ${dateCases[1][1]}`, () => {
+    const date: Date = util.getDateFromFileName(dateCases[1][0]);
+    expect(date).toEqual(new Date(dateCases[1][1]));
   });
-
-  it('\P2_9091994.ann.vcf\' -> \'Fri Sep 09 1994 00:00:00 GMT-0400\'', () => {
-    const date: Date = util.getDateFromFileName('P2_9091994.ann.vcf');
-    expect(date).toEqual(new Date('Fri Sep 09 1994 00:00:00 GMT-0400'));
+  it(`${dateCases[2][0]} -> ${dateCases[2][1]}`, () => {
+    const date: Date = util.getDateFromFileName(dateCases[2][0]);
+    expect(date).toEqual(new Date(dateCases[2][1]));
   });
-
-  it('\P3_11121993.ann.vcf\' -> \'Fri Nov 12 1993 00:00:00 GMT-0500\'', () => {
-    const date: Date = util.getDateFromFileName('P3_11121993.ann.vcf');
-    expect(date).toEqual(new Date('Fri Nov 12 1993 00:00:00 GMT-0500'));
-  });
-
-  it('\'P4.ann.vcf\' -> null', () => {
-    const date: Date = util.getDateFromFileName('P4.ann.vcf');
+  it(`${dateCases[3][0]} -> ${dateCases[3][1]}`, () => {
+    const date: Date = util.getDateFromFileName(dateCases[3][0]);
     expect(date).toEqual(null);
   });
 });
