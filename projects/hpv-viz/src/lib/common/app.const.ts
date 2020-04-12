590b1ed6f599b26d661f82ac6e073df062d21382
David Streid <david.streid.nyc@gmail.com>
2020-04-12 13:33:20 -0500

    Adding lineage-detection

--- projects/hpv-viz/src/lib/common/app.const.ts
+++ projects/hpv-viz/src/lib/common/app.const.ts
@@ -3,3 +3,14 @@
 // Fields in API for file-parsing
 export const SAMPLE_DATE = 'sampleDate';
 export const SAMPLE_NAME = 'sampleName';
+
+// VCF HEADERS
+// TODO - do a global replace
+export const VCF_CHROM = 'CHROM';
+export const VCF_POS = 'POS';
+export const VCF_ID = 'ID';
+export const VCF_REF = 'REF';
+export const VCF_ALT = 'ALT';
+export const VCF_QUAL = 'QUAL';
+export const VCF_FILTER = 'FILTER';
+export const VCF_INFO = 'INFO';
